14f65e032dcc631d7e098bc14c12a1d2c72752ee
Billy Pamanian <itisbilly@gmail.com>
2025-02-12 02:27:48 -0800

    Removed vite boilerplate

--- src/components/UserListCard.tsx
+++ src/components/UserListCard.tsx
@@ -1,11 +1,25 @@
+import { useRef, useEffect } from "react";
+
 export const UserListCard = (props: any) => {
+  const refDiv = useRef<HTMLDivElement>(null);
   const handle = props.handle;
   const avatar = props.avatar;
   const uuid = props.uuid;
   const displayName = props.displayName;
 
+  function handleClick() {
+    console.log(`${handle} has been clicked!`);
+  }
+
+  useEffect(() => {
+    refDiv.current?.addEventListener("click", handleClick);
+    return () => {
+      refDiv.current?.removeEventListener("click", handleClick);
+    }
+  }, []);
+
   return (
-    <div>
+    <div ref={refDiv}>
       <img src={avatar} />
       <p>{uuid}</p>
       <p>{displayName}</p>
