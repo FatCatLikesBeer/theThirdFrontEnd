dce146f3f665ece6d7a47c31882ca6e77567b654
Billy Pamanian <itisbilly@gmail.com>
2025-03-17 02:28:16 -0700

    Trying to get the api url handling correct

--- src/library/apiURL.ts
+++ src/library/apiURL.ts
@@ -1,7 +1,7 @@
-export default function apiURLFetcher(): "" | "http://localhost:3000" {
-  let apiURL: "" | "http://localhost:3000" = "";
-  if (document.getElementById("env")?.hasAttribute("dev")) {
-    apiURL = "http://localhost:3000";
-  }
+export default function apiURLFetcher(): string {
+  let apiURL: string = window.location.origin;
+  // if (document.getElementById("env")?.hasAttribute("dev")) {
+  //   apiURL = "http://localhost:3000";
+  // }
   return apiURL;
 }
